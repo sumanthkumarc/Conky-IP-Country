9e20f4deeb1a9e8164f99885f63283411ca17a2f
Sumanth Reddy <sumanthkumarc@users.noreply.github.com>
2020-10-05 18:48:39 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-country=$(whois ${ip} | grep country -i -m 1 |cut -d ':' -f 2 | xargs)
+#country=$(whois ${ip} | grep country -i -m 1 |cut -d ':' -f 2 | xargs)
+country=$(whois ${ip} | awk -F':[ \t]+' 'tolower($1) ~ /^country$/ { print toupper($2) }' | sed -n -e '2{p;q}')
 echo "IP: ${ip}"
 echo "Country: ${country}"
